0291227322aecc8775ad33fe9c8df893d62c6c45
troy-lamerton <troy.lamerton@gmail.com>
2016-05-18 17:07:32 +1200

    Add penguin sliding images

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ var game = new Phaser.Game(640, 400, Phaser.AUTO, 'gameContainer');
 
 var PlayingState = {
   preload: function () {
-
+    game.load.image('penguin', 'assets/images/penguin_0.png');
   },
   create: function () {
-
+    game.add.sprite(0, 0, 'penguin');
   },
   update: function () {
-
+    
   }
 };
 
